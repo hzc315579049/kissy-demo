4e6c0357951a003c558630ed51adf70ba52ba303
Carlos_Huang <315579049@qq.com>
2016-03-02 17:56:25 +0800

    3-3update
    3月3日更新

--- bower_components/kissy/build/gallary/blurWindow/index.js
+++ bower_components/kissy/build/gallary/blurWindow/index.js
@@ -1,6 +1,9 @@
 KISSY.add(function(S){
 	var $ = S.Node.all;
-
+	var DOM = S.DOM;
+	var children = DOM.children;
+	var css = DOM.css;
+	var addClass = DOM.addClass;
 	// BlurText构造器
 	// TODO BlurText工厂
 	var BlurText = function(){
@@ -38,11 +41,11 @@ KISSY.add(function(S){
 				throw new Error('Slide Container Hooker not found');
 			}
 			self.buildParams(config);
-			console.log(self);
+bBox = "."+self.blurClass;
+bText = "."+self.textClass;
 			for(var i = 0;i<self.con.length;i++){
-				
 				var node = self.con[i];
-				self.buildHTML();
+				self.buildHTML(node);
 				
 			}
 
@@ -56,7 +59,6 @@ KISSY.add(function(S){
 		},
 		buildParams: function(o){
 			var self = this;
-
 			if(o === undefined || o === null){
 				o = {};
 			}
@@ -71,19 +73,64 @@ KISSY.add(function(S){
                 speed: 100,
                 timeout: 0,
                 eventype: "hover",
-                eventClass: "blurEvent",
                 blurClass: "blurBox",
                 textClass: "blurText"
 			},setParam);
 			return self;
 		},
 		buildHTML:function(node){
-		
+			var self = this;
+			var h = DOM.height;
+			var w = DOM.width;
+			var dirc = "top";
+			var bNode = children(node,bBox);
+			var tNode = children(node,bText);
+			addClass(bNode,"blur");
+			css(node,{"overflow": "hidden"});
+			css(tNode,dirc,-h(node));
+			css(tNode,{position: 'absolute',display: 'block',height: h(node),width: w(node)});
+			css(node,{position: 'relative'})
+
+			self.buildEvent(node,h(node));
+
+		},
+		buildEvent:function(node,l){
+			var self = this;
+			var dirc = "top"
+			var speed = self.speed/10;
+			var bText = children(node,bText);
+			S.Event.on(node,"mouseenter",function(){
+				var val = l;
+				setInterval(function(){
+					if(val>0){
+						css(bText,dirc,-val+"px");
+							val=val-10;
+							console.log(val);
+						}
+					else{
+						window.clearInterval();
+					}
+				},3/19)
+				
+			})
+			S.Event.on(node,"mouseleave",function(){
+				var val = 10;
+				setInterval(function(t){
+					if(val>0){
+						css(bText,{});
+							val=val-speed;
+						}
+					else{
+						window.clearInterval()
+					}
+				},40)
+				
+			})
 		}
 
 
 	});
 	return BlurText;
 },{
-	requires:['node','json','event','anim','ua']
+	requires:['node','dom','json','event','anim','ua']
 })
